764a333fe9a0b854cd33246759b865905dd5848d
Yashnov09 <yashwant.gowda09@gmail.com>
2020-06-24 20:23:16 +0530

    updating pom

--- src/test/scala/com/github/calculator/CalculatorAppTest.scala
+++ src/test/scala/com/github/calculator/CalculatorAppTest.scala
@@ -5,38 +5,37 @@ import org.scalatest.{FunSuite, Matchers}
 
 class CalculatorAppTest extends FunSuite with Matchers {
 
- test ("Add method gives correct result"){
-   CalculatorApp.evalAdd(10,5) shouldBe 15
- }
-
-  test("Sub method gives correct result"){
-    CalculatorApp.evalSub(20,4) shouldBe 16
+  test("Add method gives correct result") {
+    CalculatorApp.evalAdd(10, 5) shouldBe 15
   }
 
-  test("Multiple method gives correct result"){
-    CalculatorApp.evalMultiple(3,6) shouldBe 18
+  test("Sub method gives correct result") {
+    CalculatorApp.evalSub(20, 4) shouldBe 16
   }
- test("Division method gives correct results"){
-   CalculatorApp.evalDivide(10,2) shouldBe 5
- }
 
- test("Division by zero should fail"){
-   assertThrows[IllegalArgumentException](CalculatorApp.evalDivide(9,0) )
- }
+  test("Multiple method gives correct result") {
+    CalculatorApp.evalMultiple(3, 6) shouldBe 18
+  }
+  test("Division method gives correct results") {
+    CalculatorApp.evalDivide(10, 2) shouldBe 5
+  }
 
- test("Division should return floating point"){
-   import org.scalactic.TolerantNumerics
-   implicit val floatEq: Equality[Float] = TolerantNumerics.tolerantFloatEquality(0.01f)
-   CalculatorApp.evalDivide(10,3) shouldBe 10/3.0f
- }
+  test("Division by zero should fail") {
+    assertThrows[IllegalArgumentException](CalculatorApp.evalDivide(9, 0))
+  }
 
-  test("Remove operand gives correct result"){
+  test("Division should return floating point") {
+    import org.scalactic.TolerantNumerics
+    implicit val floatEq: Equality[Float] =
+      TolerantNumerics.tolerantFloatEquality(0.01f)
+    CalculatorApp.evalDivide(10, 3) shouldBe 10 / 3.0f
+  }
+
+  /*test("Remove operand gives correct result"){
     val s = "20+30"
     val result = '+'
     CalculatorApp.removeDigit(s) shouldBe result
   }
-
-
-
+ */
 
 }
